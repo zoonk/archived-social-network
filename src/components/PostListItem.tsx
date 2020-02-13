d942430918d1253fbb518f201a2fdc1d2ed72919
wceolin <wceolin@gmail.com>
2020-02-13 17:03:05 -0300

    add topics to post list

--- src/components/PostListItem.tsx
+++ src/components/PostListItem.tsx
@@ -1,12 +1,16 @@
 import {
   Avatar,
+  Link,
   ListItem,
   ListItemAvatar,
   ListItemText,
+  Typography,
 } from '@material-ui/core';
+import { Description } from '@material-ui/icons';
 import NextLink from 'next/link';
 import { Post } from '@zoonk/models';
-import { getPostImage } from '@zoonk/utils';
+import { getPostImage, theme } from '@zoonk/utils';
+import TopicLabel from './TopicLabel';
 
 interface PostListItemProps {
   divider?: boolean;
@@ -17,32 +21,32 @@ interface PostListItemProps {
  * Display a single post as a list item.
  */
 const PostListItem = ({ divider, item }: PostListItemProps) => {
-  const { chapter, content, createdBy, path, title } = item;
-  const image =
-    getPostImage(content) || chapter?.photo || path?.photo || createdBy?.photo;
+  const { chapter, content, path, title, topics } = item;
+  const image = getPostImage(content) || chapter?.photo || path?.photo;
 
   return (
-    <NextLink href="/posts/[id]" as={`/posts/${item.id}`} passHref>
-      <ListItem
-        alignItems="flex-start"
-        button
-        component="a"
-        divider={divider}
-        disableGutters
-      >
-        <ListItemAvatar>
-          <Avatar src={image || undefined} />
-        </ListItemAvatar>
-        <ListItemText
-          primary={title}
-          secondary={content.slice(0, 200)}
-          secondaryTypographyProps={{
-            gutterBottom: false,
-            noWrap: true,
-          }}
-        />
-      </ListItem>
-    </NextLink>
+    <ListItem alignItems="flex-start" divider={divider} disableGutters>
+      <ListItemAvatar>
+        <Avatar
+          src={image || undefined}
+          style={{ backgroundColor: theme.palette.primary.main }}
+        >
+          <Description />
+        </Avatar>
+      </ListItemAvatar>
+      <ListItemText
+        primary={
+          <NextLink href="/posts/[id]" as={`/posts/${item.id}`} passHref>
+            <Link color="textPrimary">
+              <Typography gutterBottom>{title}</Typography>
+            </Link>
+          </NextLink>
+        }
+        secondary={topics.slice(0, 3).map((id) => (
+          <TopicLabel key={id} id={id} />
+        ))}
+      />
+    </ListItem>
   );
 };
 
---
+++ src/components/TopicLabel.tsx
@@ -0,0 +1,27 @@
+import NextLink from 'next/link';
+import { Chip } from '@material-ui/core';
+import { getPageTitle, theme } from '@zoonk/utils';
+
+interface TopicLabelProps {
+  id: string;
+}
+
+/**
+ * Display a topic label.
+ */
+const TopicLabel = ({ id }: TopicLabelProps) => {
+  return (
+    <NextLink key={id} href="/topics/[id]" as={`/topics/${id}`} passHref>
+      <Chip
+        component="a"
+        size="small"
+        variant="outlined"
+        color="primary"
+        label={getPageTitle(id)}
+        style={{ marginRight: theme.spacing(0.5) }}
+      />
+    </NextLink>
+  );
+};
+
+export default TopicLabel;
