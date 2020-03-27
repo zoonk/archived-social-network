293da388c175344dcc42abfdddcbf155304b6ee2
wceolin <wceolin@gmail.com>
2020-03-27 13:23:20 -0300

    change order for images and title on link metadata function

--- functions/src/helpers/url.ts
+++ functions/src/helpers/url.ts
@@ -6,20 +6,34 @@ export const getMetadataFromUrl = async (url: string): Promise<Post.Link> => {
   const response = await fetch(url);
   const html = await response.text();
   const $ = cheerio.load(html);
+  // Remove trailling slash (/) from URL
+  const link = url.replace(/\/+$/, '');
   const title = $('title').text();
   const description = $('meta[name="description"]').attr('content');
   const ogTitle = $('meta[property="og:title"]').attr('content');
   const ogDesc = $('meta[property="og:description"]').attr('content');
   const ogImage = $('meta[property="og:image"]').attr('content');
   const canonical = $('link[rel="canonical"]').attr('href');
-  const shortcut = $('link[rel="shortcut icon"]').attr('href');
-  const icon = $('link[rel="icon"]').attr('href');
-  const appleIcon = $('link[rel="apple-touch-icon"]').attr('href');
+  let shortcut = $('link[rel="shortcut icon"]').attr('href');
+  let icon = $('link[rel="icon"]').attr('href');
+  let appleIcon = $('link[rel="apple-touch-icon"]').attr('href');
+
+  if (shortcut?.startsWith('/')) {
+    shortcut = link + shortcut;
+  }
+
+  if (icon?.startsWith('/')) {
+    icon = link + icon;
+  }
+
+  if (appleIcon?.startsWith('/')) {
+    appleIcon = link + appleIcon;
+  }
 
   return {
     description: description || ogDesc || null,
-    image: ogImage || shortcut || icon || appleIcon || null,
-    title: title || ogTitle || 'undefined',
+    image: ogImage || appleIcon || shortcut || icon || null,
+    title: ogTitle || title || 'undefined',
     url: canonical || url,
   };
 };
