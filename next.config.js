ffb3456bc3942f1d2b2c979bf5c7ec4f0af0ead7
wceolin <wceolin@gmail.com>
2020-05-29 20:59:52 -0300

    enable service workers

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
+const withOffline = require('next-offline');
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
 
-const nextConfig = {};
+const nextConfig = {
+  workboxOpts: {
+    // Move the service worker to the public folder.
+    // Otherwise, we'll get a 404 error in production.
+    swDest: 'static/service-worker.js',
+  },
+};
 
-module.exports = withBundleAnalyzer(nextConfig);
+module.exports = withOffline(withBundleAnalyzer(nextConfig));
