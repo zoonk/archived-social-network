1371967006de7eb219cc64eec0a197251e1d64ac
wceolin <wceolin@gmail.com>
2020-06-10 22:05:59 -0300

    Remove material-ui locale
    Reduce bundle size by 6.09%

--- src/utils/theme.ts
+++ src/utils/theme.ts
@@ -1,36 +1,20 @@
 import { createMuiTheme } from '@material-ui/core/styles';
 import { blue, green, pink, red } from '@material-ui/core/colors';
-import { enUS, ptBR } from '@material-ui/core/locale';
-import { appLanguage } from './settings';
-
-const getMaterialLanguage = () => {
-  switch (appLanguage) {
-    case 'en':
-      return enUS;
-    case 'pt':
-      return ptBR;
-    default:
-      return enUS;
-  }
-};
 
 // Create a theme instance.
-export const theme = createMuiTheme(
-  {
-    palette: {
-      primary: blue,
-      secondary: pink,
-      error: {
-        main: red.A400,
-      },
-      background: {
-        default: '#fff',
-      },
-      success: {
-        contrastText: 'white',
-        main: green[400],
-      },
+export const theme = createMuiTheme({
+  palette: {
+    primary: blue,
+    secondary: pink,
+    error: {
+      main: red.A400,
+    },
+    background: {
+      default: '#fff',
+    },
+    success: {
+      contrastText: 'white',
+      main: green[400],
     },
   },
-  getMaterialLanguage(),
-);
+});
