ca1252d55f2e27d659019cfa6e32289d13a7f3a4
wceolin <wceolin@gmail.com>
2020-06-18 12:01:28 -0300

    add custom paths to jest config

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,8 @@
 module.exports = {
   preset: 'ts-jest',
   setupFiles: ['core-js'],
+  moduleNameMapper: {
+    // https://github.com/kulshekhar/ts-jest/issues/414
+    '@zoonk/(.*)': '<rootDir>/src/$1',
+  },
 };
