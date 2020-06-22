4a3dc8d269085f9bcdd1666be839d3100fdedff6
wceolin <wceolin@gmail.com>
2020-06-22 17:45:29 -0300

    fix jest errors on node environment

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   preset: 'ts-jest',
   setupFiles: ['core-js'],
+  testEnvironment: 'node',
   moduleNameMapper: {
     // https://github.com/kulshekhar/ts-jest/issues/414
     '@zoonk/(.*)': '<rootDir>/src/$1',
