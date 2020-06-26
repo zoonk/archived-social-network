631bf44042813b123ee1f5ee72cce8dff22f9c8c
wceolin <wceolin@gmail.com>
2020-06-26 13:28:44 -0300

    use ssr for topic page

--- src/pages/topics/[id]/index.tsx
+++ src/pages/topics/[id]/index.tsx
@@ -1,7 +1,6 @@
-import { GetStaticPaths, GetStaticProps, InferGetStaticPropsType } from 'next';
+import { GetServerSideProps, InferGetServerSidePropsType } from 'next';
 import Error from 'next/error';
-import { useRouter } from 'next/router';
-import { CircularProgress, Container } from '@material-ui/core';
+import { Container } from '@material-ui/core';
 import ChaptersCard from '@zoonk/components/ChaptersCard';
 import Meta from '@zoonk/components/Meta';
 import PostsList from '@zoonk/components/PostsList';
@@ -10,7 +9,7 @@ import TopicBase from '@zoonk/components/TopicBase';
 import TopicsBreadcrumb from '@zoonk/components/TopicsBreadcrumb';
 import useTranslation from '@zoonk/components/useTranslation';
 import { Post, Topic } from '@zoonk/models';
-import { getPosts, getTopic, getTopics } from '@zoonk/services';
+import { getPosts, getTopic } from '@zoonk/services';
 
 interface TopicPageProps {
   posts: Post.Get[];
@@ -19,31 +18,23 @@ interface TopicPageProps {
 
 const limit = 10;
 
-export const getStaticPaths: GetStaticPaths = async () => {
-  const topics = await getTopics({ limit: 20 });
-  const paths = topics.map((topic) => ({ params: { id: topic.id } }));
-  return { paths, fallback: true };
-};
-
-export const getStaticProps: GetStaticProps<TopicPageProps> = async ({
+export const getServerSideProps: GetServerSideProps<TopicPageProps> = async ({
   params,
 }) => {
   const topicId = String(params?.id);
   const topicReq = getTopic(topicId);
   const postsReq = getPosts({ topicId, limit });
   const [topic, posts] = await Promise.all([topicReq, postsReq]);
-  return { props: { posts, topic }, unstable_revalidate: 1 };
+  return { props: { posts, topic } };
 };
 
 const TopicPage = ({
   posts,
   topic,
-}: InferGetStaticPropsType<typeof getStaticProps>) => {
+}: InferGetServerSidePropsType<typeof getServerSideProps>) => {
   const translate = useTranslation();
-  const { isFallback } = useRouter();
 
-  if (!topic && !isFallback) return <Error statusCode={404} />;
-  if (!topic) return <CircularProgress />;
+  if (!topic) return <Error statusCode={404} />;
 
   const { chapterData, id, language, photo, title } = topic;
 
